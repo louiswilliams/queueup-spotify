34314d57963cab6cc6b21170a4fc11b6a511bf66
Louis Williams <louieaw@gmail.com>
2015-08-11 12:31:34 -0400

    Optimize alert for search

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
+    $alert = $("<div id='app_alert'>").addClass("alert").addClass("alert-warning").addClass("alert-dismissable");
+
+    $alert_close = $("<a class='close' href='#' data-dismiss='alert' aria-label='close'>X</a>");
+
+    var message = "Unfortunately, Spotify doesn't allow streaming " +
+        "in web apps. Download our app on iOS or Android " + 
+        "to host a playlist! ";
+
+    $alert.html(message);
+    $alert.append($alert_close);
+
+ 
     $(".get_app_question").click(function (e) {
         e.preventDefault();
-        $("#app_alert").show();
+        $('#playlist_listing').before($alert); 
+        $alert.slideDown(200);
     })
 });
